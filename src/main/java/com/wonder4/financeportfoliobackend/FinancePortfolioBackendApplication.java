9831130c66b5a98ee1de6004471fd5f10b9de00a
Vincent M <keel_direst_0r@icloud.com>
2026-03-30 14:48:54 +0800

    Initialize springboot project

---
+++ src/main/java/com/wonder4/financeportfoliobackend/FinancePortfolioBackendApplication.java
@@ -0,0 +1,12 @@
+package com.wonder4.financeportfoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancePortfolioBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinancePortfolioBackendApplication.class, args);
+  }
+}
---
+++ src/test/java/com/wonder4/financeportfoliobackend/FinancePortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonder4.financeportfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancePortfolioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
