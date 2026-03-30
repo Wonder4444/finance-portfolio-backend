6d715b157d7359d5d1cd17b845fc2420f9cb3f65
Vincent M <keel_direst_0r@icloud.com>
2026-03-30 22:32:41 +0800

    Test DB connection

---
+++ src/test/java/com/wonder4/finance_portfolio_backend/DatabaseConnectionTest.java
@@ -0,0 +1,29 @@
+package com.wonder4.finance_portfolio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class DatabaseConnectionTest {
+
+  @Autowired private DataSource dataSource;
+
+  @Test
+  void testConnection() throws SQLException {
+    System.out.println("start");
+    try (Connection connection = dataSource.getConnection()) {
+      assertNotNull(connection);
+      assertTrue(connection.isValid(1));
+      System.out.println("Connection built: " + connection.getMetaData().getURL());
+    }
+    System.out.println("end");
+  }
+}
