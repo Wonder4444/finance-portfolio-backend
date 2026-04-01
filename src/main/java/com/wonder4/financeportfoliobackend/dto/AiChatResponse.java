58731c8f228a3c48367c1de74ad12faf1300436e
Vincent M <keel_direst_0r@icloud.com>
2026-04-01 11:36:54 +0800

    Feat: Ollama integration and portfolio-aware context management

---
+++ src/main/java/com/wonder4/financeportfoliobackend/dto/AiChatResponse.java
@@ -0,0 +1,16 @@
+package com.wonder4.financeportfoliobackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AiChatResponse {
+
+    private String chatId;
+    private String reply;
+}
