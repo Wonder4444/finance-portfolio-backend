4af3965d76c730bc196d7c594885a4ee1a85e056
Vincent M <keel_direst_0r@icloud.com>
2026-04-01 11:37:28 +0800

    End point for ai chat

---
+++ src/main/java/com/wonder4/financeportfoliobackend/controller/AiChatController.java
@@ -0,0 +1,28 @@
+package com.wonder4.financeportfoliobackend.controller;
+
+import com.wonder4.financeportfoliobackend.dto.AiChatRequest;
+import com.wonder4.financeportfoliobackend.dto.AiChatResponse;
+import com.wonder4.financeportfoliobackend.service.AiChatService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ai")
+public class AiChatController {
+
+    private final AiChatService aiChatService;
+
+    public AiChatController(AiChatService aiChatService) {
+        this.aiChatService = aiChatService;
+    }
+
+    @PostMapping("/chat")
+    public ResponseEntity<AiChatResponse> chat(@RequestBody AiChatRequest request) {
+        AiChatResponse response = aiChatService.chat(request);
+        return ResponseEntity.ok(response);
+    }
+}
